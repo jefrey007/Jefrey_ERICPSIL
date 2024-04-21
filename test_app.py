554907cbea6267988a1b58d156ead88ea4f4c43c
jefrey007 <95954356+jefrey007@users.noreply.github.com>
2024-04-21 12:05:30 +0530

    Create test_app.py

---
+++ test_app.py
@@ -0,0 +1,19 @@
+# test_app.py
+import unittest
+from app import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_login_page(self):
+        response = self.app.get('/')
+        self.assertIn(b"Login Page", response.data)
+
+if __name__ == "__main__":
+    unittest.main()
